d012a05bd8cb2fa9f86d99bdfa9d0e3e13f29ee7
helloworldzlg <helloworldzlg@gmail.com>
2016-07-16 11:55:24 +0800

    modify emotion.cpp

--- emotion/emotion.cpp
+++ emotion/emotion.cpp
@@ -20,32 +20,43 @@ extern "C"
 #define EMOTION_LOOKRIGHT            (7)
 #define EMOTION_TEARS                (8)
 
+int g_uart2_fileid;
 int g_uart3_fileid;
 
 int SerialPort_init()
 {
     int fd = -1;
-    fd = open("/dev/ttyAMA3", O_RDWR | O_NOCTTY | O_NDELAY);
-    if (fd == -1) {
-        perror("Open Serial Port Error!\n");
-        return -1;
-    }
-
     struct termios options;
-    tcgetattr(fd, &options);
-
+    
     //115200, 8N1
     options.c_cflag = B115200 | CS8 | CLOCAL | CREAD;
     options.c_iflag = IGNPAR;
     options.c_oflag = 0;
     options.c_lflag = 0;
     options.c_cc[VTIME]=0;
-    options.c_cc[VMIN]=1;
-    tcflush(fd, TCIFLUSH);
+    options.c_cc[VMIN]=1;    
 
-    tcsetattr(fd, TCSANOW, &options);
+    fd = open("/dev/ttyAMA2", O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd == -1) {
+        perror("Open Serial Port Error!\n");
+        return -1;
+    }
 
-    g_uart3_fileid = fd;
+	tcgetattr(fd, &options);
+	tcflush(fd, TCIFLUSH);
+	tcsetattr(fd, TCSANOW, &options);
+	g_uart2_fileid = fd;
+
+    fd = open("/dev/ttyAMA3", O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd == -1) {
+        perror("Open Serial Port Error!\n");
+        return -1;
+    }
+
+	tcgetattr(fd, &options);
+	tcflush(fd, TCIFLUSH);
+	tcsetattr(fd, TCSANOW, &options);
+	g_uart3_fileid = fd;
 
     return 0;
 }
@@ -55,26 +66,31 @@ void emotion_blink()
 	unsigned char tx_buffer[] = {0xAA, 0x70, 0x00, 0xCC, 0x33, 0xC3, 0x3C};
 	
 	tx_buffer[2] = 0x00;
+	write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	
 	usleep(400000);
 	
 	tx_buffer[2] = 0x01;
+	write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));	
 	
 	usleep(400000);
 	
 	tx_buffer[2] = 0x00;
+	write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	
 	usleep(400000);
 	
 	tx_buffer[2] = 0x01;
+	write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));	
 	
 	usleep(400000);
 	
 	tx_buffer[2] = 0x00;
+	write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));		
 }
 
@@ -86,27 +102,32 @@ void emotion_pleasure()
 	for (index = 0; index < 2; index++)
 	{
 		tx_buffer[2] = 0x02;
+		write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 		write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	
 		usleep(400000);
 
 		tx_buffer[2] = 0x03;
+		write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 		write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	
 		usleep(400000);
 
 		tx_buffer[2] = 0x04;
+		write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 		write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	
 		usleep(400000);
 
 		tx_buffer[2] = 0x05;
+		write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 		write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	
 		usleep(400000);
 	}
 
 	tx_buffer[2] = 0x00;
+	write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));	
 }
 
@@ -115,6 +136,7 @@ void emotion_anger()
 	unsigned char tx_buffer[] = {0xAA, 0x70, 0x00, 0xCC, 0x33, 0xC3, 0x3C};
 
 	tx_buffer[2] = 0x06;
+	write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 
 }
@@ -127,17 +149,20 @@ void emotion_sorrow()
 	for (index = 0; index < 2; index++)
 	{
 		tx_buffer[2] = 0x07;
+		write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 		write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 
 		usleep(400000);
 
 		tx_buffer[2] = 0x08;
+		write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 		write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 
 		usleep(400000);
 	}
 
 	tx_buffer[2] = 0x00;
+	write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));	
 }
 
@@ -151,6 +176,7 @@ void emotion_lookleft()
 	unsigned char tx_buffer[] = {0xAA, 0x70, 0x00, 0xCC, 0x33, 0xC3, 0x3C};
 
 	tx_buffer[2] = 0x09;
+	write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 }
 
@@ -159,6 +185,7 @@ void emotion_lookright()
 	unsigned char tx_buffer[] = {0xAA, 0x70, 0x00, 0xCC, 0x33, 0xC3, 0x3C};
 
 	tx_buffer[2] = 0xa;
+	write(g_uart2_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 	write(g_uart3_fileid, (void*)tx_buffer, sizeof(tx_buffer));
 }
 
@@ -208,7 +235,7 @@ int Robot_SetEmotion(int emotion_id)
 			break;
 
 		case EMOTION_TEARS:
-			emotion_tears();
+			//emotion_tears();
 			break;
 
 		default:
